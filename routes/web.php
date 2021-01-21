36ccd57f8e5fcf947f70fc1d637f63d25dbe4095
Armaine Cervantes <chavez.rmaine@gmail.com>
2021-01-21 20:10:01 +0800

    modified the default route

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/tasks', [TaskController::class, 'index']);
+// Route::get('/tasks', [TaskController::class, 'index']);
+Route::get('/', [TaskController::class, 'index']);
 
 Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
 
